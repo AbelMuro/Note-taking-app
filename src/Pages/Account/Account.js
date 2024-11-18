b74875f1cdf0197610f6db06cedcc2caf35d9c01
abelmuro <abelmuro93@gmail.com>
2024-11-17 17:26:41 -0800

    commit

--- src/Pages/Account/Account.js
+++ src/Pages/Account/Account.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import * as styles from './styles.module.css';
 
+//this is where i left off
 function Account() {
 
     const getNotes = async () => {
