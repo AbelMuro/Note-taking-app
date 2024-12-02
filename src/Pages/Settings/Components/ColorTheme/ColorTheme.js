a21e3940179ecd376d41f3c27e9fab6c87ddc266
abelmuro <abelmuro93@gmail.com>
2024-12-01 17:15:00 -0800

    commit

--- src/Pages/Settings/Components/ColorTheme/ColorTheme.js
+++ src/Pages/Settings/Components/ColorTheme/ColorTheme.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import * as styles from './styles.module.css';
 
+//this is where i left off, i will need to implement the colorTheme route and change the theme in the navigation bar of the settings component
 function ColorTheme(){
     return(<></>)
 }
