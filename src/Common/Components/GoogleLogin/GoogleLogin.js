82c005a6f6503698d5c6e23aa9bf13341f362cdd
AbelMuro <abelmuro93@gmail.com>
2024-12-16 17:30:23 -0800

    commit

--- src/Common/Components/GoogleLogin/GoogleLogin.js
+++ src/Common/Components/GoogleLogin/GoogleLogin.js
@@ -6,36 +6,17 @@ import * as styles from './styles.module.css';
 function GoogleLoginButton() {
     const [theme, changeClass] = useTheme(styles);
 
-    const handleCredentialResponse = (res) => {
-        console.log(res.credential);
+
+    const handleGoogle = () => {
+        
     }
-
-    const triggerGoogleSignIn = () => { 
-        window.google.accounts.id.prompt((notification) => { 
-            if(notification.isNotDisplayed()) 
-                console.log(notification.getNotDisplayedReason()); 
-            else if(notification.isSkippedMoment()) 
-                console.log(notification.getSkippedReason()); 
-            else if(notification.isDismissedMoment()) 
-                console.log(notification.getDismissedReason())
-            }); 
-    };
-
-
-    useEffect(() => {
-        if(!window.google) return;
-        window.google.accounts.id.initialize({ 
-            client_id: process.env.CLIENT_ID, 
-            callback: handleCredentialResponse, 
-        });
-    }, [window.google])
 
     return(
             <section className={changeClass('container')}>
                 <p className={changeClass('container_title')}>
                     Or log in with:
                 </p>
-                <button className={changeClass('container_button')} onClick={triggerGoogleSignIn}>
+                <button className={changeClass('container_button')} onClick={handleGoogle}>
                     <img src={theme === 'light' ? icons['google'] : icons['googleDark']}/>
                     Google
                 </button>
