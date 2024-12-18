55312f4197852e62123f9708928628cf3a3c04f9
AbelMuro <abelmuro93@gmail.com>
2024-12-18 14:59:07 -0800

    commit

--- src/Common/Components/GoogleLoginButton/GoogleLoginButton.js
+++ src/Common/Components/GoogleLoginButton/GoogleLoginButton.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import {useNavigate} from 'react-router-dom';
 import { useGoogleLogin } from '@react-oauth/google';
 import {useTheme} from '~/Hooks';
 import icons from './icons';
 import * as styles from './styles.module.css';
 
 function GoogleLoginButton() {
+    const navigate = useNavigate();
     const login = useGoogleLogin({
         onSuccess: async (token) => {
            const response = await fetch('https://note-taking-server-thr9.onrender.com/google-login', {
@@ -12,8 +14,19 @@ function GoogleLoginButton() {
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify(token)
+            body: JSON.stringify({token})
            })
+
+           if(response.status === 200){
+                const result = await response.text();
+                console.log(result);
+                navigate('/account');
+           }
+           else{
+            const message = await response.text();
+            const event = new CustomEvent('display-message', {'detail' : {message}});
+            document.dispatchEvent(event);
+           }
         }
     })
     const [theme, changeClass] = useTheme(styles);
