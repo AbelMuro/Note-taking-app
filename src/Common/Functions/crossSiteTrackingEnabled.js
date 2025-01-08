d69e48ed5530f627fb350b40b096b1f837c2ee3e
AbelMuro <abelmuro93@gmail.com>
2025-01-07 16:36:49 -0800

    commit

--- src/Common/Functions/crossSiteTrackingEnabled.js
+++ src/Common/Functions/crossSiteTrackingEnabled.js
@@ -1,7 +1,7 @@
 const crossSiteTrackingEnabled = () => {
     const doNotTrackEnabled = (navigator.doNotTrack === "1") || (navigator.msDoNotTrack === "1");
 
-    if(!doNotTrackEnabled){
+    if(doNotTrackEnabled){
         const event = new CustomEvent('display-message', {'detail' : {message: 'Please enable cross-site-tracking in your browser to use this app', error: true}});
         document.dispatchEvent(event);
         return false;
