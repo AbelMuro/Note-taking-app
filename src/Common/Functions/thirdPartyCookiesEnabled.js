66a6bafab1207c52a33bc482c5abca43b4100df9
AbelMuro <abelmuro93@gmail.com>
2025-01-07 16:50:17 -0800

    commit

--- src/Common/Functions/thirdPartyCookiesEnabled.js
+++ src/Common/Functions/thirdPartyCookiesEnabled.js
@@ -1,6 +1,7 @@
 const thirdPartyCookiesEnabled = () => {
     document.cookie = 'thirdPartyTestCookie=test; SameSite=None; Secure';
     const cookieExists = document.cookie.indexOf("thirdPartyTestCookie") !== -1;
+    console.log(cookieExists, document.cookie.indexOf("thirdPartyTestCookie"));
 
     if(!cookieExists){
         const event = new CustomEvent('display-message', {'detail' : {message: 'Please enable third-party-cookies in your browser to use this app', error: true}})
